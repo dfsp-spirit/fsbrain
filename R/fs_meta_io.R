41ec348c5a268168ae1933fc138bcda1f0407ee1
Tim Schaefer <ts+code@rcmd.org>
2019-09-17 18:03:42 +0200

    add untested function to read demographcis

--- R/fs_meta_io.R
+++ R/fs_meta_io.R
@@ -1,6 +1,29 @@
-
+#' @title Read subjects file
+#'
+#' @description Load a list of subjects from a subjects file, i.e., a simple text file containing one subject name per line and no header.
+#'
+#' @param subjects_file, string. The path to the file.
+#'
+#' @return a vector of strings. The subject IDs.
+#'
+#' @export
 nit.read.subjectsfile = function(subjects_file) {
     subjects_df = utils::read.table(subjects_file, header=FALSE, col.names = c("subject_id"));
     subjects_list = as.vector(subjects_df$subject_id);
     return(subjects_list);
+}
+
+#' @title Read demographics file
+#'
+#' @description Load a list of subjects and metadata from a demographics file, i.e., a tab-separated file containing an arbitrary number of columns, one of which must be the subject id.
+#'
+#' @param demographics_file, string. The path to the file.
+#'
+#' @return a data.frame. The data in the file.
+#'
+#' @export
+nit.read.demographics = function(demographics_file) {
+  demographics_df = utils::read.table(demographics_file, header=TRUE, sep='\t');
+  demographics_df = demographics %>% mutate_if(is.character, as.factor) %>% mutate_if(is.numeric, scale, scale=F);
+  return(demographics_df);
 }
