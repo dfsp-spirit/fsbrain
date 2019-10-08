761f80f2e9ef688c069e7cc21e6952e6702ca863
Tim Schäfer <ts+code@rcmd.org>
2019-10-08 19:28:55 +0200

    add unit test for error on wrong column count

--- tests/testthat/test-f-meta-io.R
+++ tests/testthat/test-f-meta-io.R
@@ -4,9 +4,17 @@ test_that("Subjects file can  be read", {
     expect_equal(length(subjects_list), 3);
 })
 
+
 test_that("Demographics file can  be read", {
   demogr_file = system.file("extdata", "demographics.tsv", package = "nitools", mustWork = TRUE);
   column_names = c("subject_id", "group", "age");
   demographics = read.demographics(demogr_file, column_names = column_names, report = FALSE);
   expect_equal(nrow(demographics), 4);
+})
+
+
+test_that("Demographics file reading fails with incorrect number of header names", {
+  demogr_file = system.file("extdata", "demographics.tsv", package = "nitools", mustWork = TRUE);
+  column_names = c("subject_id", "group", "age", "nosuchfield");
+  expect_error(demographics = read.demographics(demogr_file, column_names = column_names, report = FALSE));
 })
