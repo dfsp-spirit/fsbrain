5c9c015cfa7a6ff04822ced77465f42fcd17b6ed
Tim Schäfer <ts+code@rcmd.org>
2019-10-11 12:00:33 +0200

    Support hemi setting 'both' for global aggregations.

--- R/fs_agg.R
+++ R/fs_agg.R
@@ -116,7 +116,7 @@ subject.morph.standard <- function(subjects_dir, subject_id, measure, hemi, fwhm
 #'
 #' @param measure, string. Name of the vertex-wise measure of morphometry data file. E.g., "area" or "thickness". Used to construct the name of the morphometry file to be loaded.
 #'
-#' @param hemi, string, one of 'lh' or 'rh'. The hemisphere name. Used to construct the names of the annotation and morphometry data files to be loaded.
+#' @param hemi, string, one of 'lh', 'rh' or 'both'. The hemisphere name. Used to construct the names of the annotation and morphometry data files to be loaded.
 #'
 #' @param agg_fun, function. An R function that aggregates data, typically max, mean, min or something similar. Note: this is NOT a string, put the function name without quotes. Defaults to mean.
 #'
@@ -129,8 +129,8 @@ subject.morph.standard <- function(subjects_dir, subject_id, measure, hemi, fwhm
 #'
 #' @export
 group.morph.agg.native <- function(subjects_dir, subjects_list, measure, hemi, agg_fun = mean, cast=TRUE, format='curv') {
-  if(!(hemi %in% c("lh", "rh"))) {
-    stop(sprintf("Parameter 'hemi' must be one of 'lh' or 'rh' but is '%s'.\n", hemi));
+  if(!(hemi %in% c("lh", "rh", "both"))) {
+    stop(sprintf("Parameter 'hemi' must be one of 'lh', 'rh' or 'both but is '%s'.\n", hemi));
   }
   agg_all_subjects = data.frame();
   for (subject_id in subjects_list) {
@@ -176,7 +176,7 @@ group.morph.agg.native <- function(subjects_dir, subjects_list, measure, hemi, a
 #'
 #' @param measure, string. Name of the vertex-wise measure of morphometry data file. E.g., "area" or "thickness". Used to construct the name of the morphometry file to be loaded.
 #'
-#' @param hemi, string, one of 'lh' or 'rh'. The hemisphere name. Used to construct the names of the annotation and morphometry data files to be loaded.
+#' @param hemi, string, one of 'lh', 'rh' or 'both'. The hemisphere name. Used to construct the names of the annotation and morphometry data files to be loaded.
 #'
 #' @param fwhm, string. Smoothing as string, e.g. '10' or '25'.
 #'
@@ -193,8 +193,8 @@ group.morph.agg.native <- function(subjects_dir, subjects_list, measure, hemi, a
 #'
 #' @export
 group.morph.agg.standard <- function(subjects_dir, subjects_list, measure, hemi, fwhm, agg_fun = mean, template_subject='fsaverage', format='mgh', cast=TRUE) {
-  if(!(hemi %in% c("lh", "rh"))) {
-    stop(sprintf("Parameter 'hemi' must be one of 'lh' or 'rh' but is '%s'.\n", hemi));
+  if(!(hemi %in% c("lh", "rh", "both"))) {
+    stop(sprintf("Parameter 'hemi' must be one of 'lh', 'rh' or 'both but is '%s'.\n", hemi));
   }
   agg_all_subjects = data.frame();
   for (subject_id in subjects_list) {
@@ -257,8 +257,8 @@ group.morph.agg.standard <- function(subjects_dir, subjects_list, measure, hemi,
 group.multimorph.agg.standard <- function(subjects_dir, subjects_list, measures, hemis, fwhm, agg_fun = mean, template_subject='fsaverage', format='mgh', cast=TRUE) {
   agg_all_measures_and_hemis = data.frame();
   for (hemi in hemis) {
-    if(!(hemi %in% c("lh", "rh"))) {
-      stop(sprintf("Each entry in the parameter 'hemis' must be one of 'lh' or 'rh', but the current one is '%s'.\n", hemi));
+    if(!(hemi %in% c("lh", "rh", "both"))) {
+      stop(sprintf("Each entry in the parameter 'hemis' must be one of 'lh', 'rh' or 'both, but the current one is '%s'.\n", hemi));
     }
     for (measure in measures) {
         measure_hemi_data = group.morph.agg.standard(subjects_dir, subjects_list, measure, hemi, fwhm, agg_fun = agg_fun, template_subject=template_subject, format=format, cast=cast);
@@ -306,8 +306,8 @@ group.multimorph.agg.standard <- function(subjects_dir, subjects_list, measures,
 group.multimorph.agg.native <- function(subjects_dir, subjects_list, measures, hemis, agg_fun = mean, format='curv', cast=TRUE) {
   agg_all_measures_and_hemis = data.frame();
   for (hemi in hemis) {
-    if(!(hemi %in% c("lh", "rh"))) {
-      stop(sprintf("Each entry in the parameter 'hemis' must be one of 'lh' or 'rh', but the current one is '%s'.\n", hemi));
+    if(!(hemi %in% c("lh", "rh", "both"))) {
+      stop(sprintf("Each entry in the parameter 'hemis' must be one of 'lh', 'rh' or 'both' but the current one is '%s'.\n", hemi));
     }
     for (measure in measures) {
       measure_hemi_data = group.morph.agg.native(subjects_dir, subjects_list, measure, hemi, agg_fun = agg_fun, format=format, cast=cast);
