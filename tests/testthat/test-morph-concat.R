9a53d0fd247d702fe91bc3218acc83c800ff9cde
Tim Schaefer <ts+code@rcmd.org>
2019-10-15 12:31:46 +0200

    use optdata in more tests, remove need for volume files

--- tests/testthat/test-morph-concat.R
+++ tests/testthat/test-morph-concat.R
@@ -1,67 +1,66 @@
 test_that("Concatination of native space measures works for a single hemisphere", {
-    subjects_dir = path.expand("~/data/tim_only")
+    nitools::download_optional_data();
+    subjects_dir = nitools::get_optional_data_filepath("subjects_dir");
     skip_if_not(dir.exists(subjects_dir), message="Test data missing.") # skip on travis
 
     num_verts_subject1_lh = 149244;
     num_verts_subject1_rh = 153333;
 
-    subjects_list = c("tim", "timcopy");
-    measures = c("area", "thickness", "volume");
+    subjects_list = c("subject1", "subject2");
+    measures = c("area", "thickness");
     mc = group.concat.measures.native(subjects_dir, subjects_list, measures, "lh");
     expect_equal(nrow(mc), num_verts_subject1_lh * 2 * 1);   # 2 subjects with 1 hemi each
-    expect_equal(ncol(mc), 3);  # 3 measures
+    expect_equal(ncol(mc), 2);  # 2 measures
     expect_true("area" %in% colnames(mc));
-    expect_true("volume" %in% colnames(mc));
     expect_true("thickness" %in% colnames(mc));
 })
 
 test_that("Concatination of native space measures works for both hemispheres", {
-    subjects_dir = path.expand("~/data/tim_only")
+    nitools::download_optional_data();
+    subjects_dir = nitools::get_optional_data_filepath("subjects_dir");
     skip_if_not(dir.exists(subjects_dir), message="Test data missing.") # skip on travis
 
     num_verts_subject1_lh = 149244;
     num_verts_subject1_rh = 153333;
     num_verts_subject1_both = num_verts_subject1_lh + num_verts_subject1_rh;
 
-    subjects_list = c("tim", "timcopy");
-    measures = c("area", "thickness", "volume");
+    subjects_list = c("subject1", "subject2");
+    measures = c("area", "thickness");
     mc = group.concat.measures.native(subjects_dir, subjects_list, measures, "both");
     expect_equal(nrow(mc), num_verts_subject1_both * 2);   # 2 subjects with 1 hemi each
-    expect_equal(ncol(mc), 3);  # 3 measures
+    expect_equal(ncol(mc), 2);  # 2 measures
     expect_true("area" %in% colnames(mc));
-    expect_true("volume" %in% colnames(mc));
     expect_true("thickness" %in% colnames(mc));
 })
 
 
 test_that("Concatination of standard space measures works for a single hemisphere", {
-    subjects_dir = path.expand("~/data/tim_only")
+    nitools::download_optional_data();
+    subjects_dir = nitools::get_optional_data_filepath("subjects_dir");
     skip_if_not(dir.exists(subjects_dir), message="Test data missing.") # skip on travis
 
     num_verts_fsaverage_per_hemi = 163842;
 
-    subjects_list = c("tim", "timcopy");
-    measures = c("area", "thickness", "volume");
+    subjects_list = c("subject1", "subject2");
+    measures = c("area", "thickness");
     mc = group.concat.measures.standard(subjects_dir, subjects_list, measures, "lh", fwhm="10");
     expect_equal(nrow(mc), num_verts_fsaverage_per_hemi * 2 * 1);  # 2 subjects with 1 hemi each
-    expect_equal(ncol(mc), 3); # 3 measures
-    expect_true("area_fwhm10" %in% colnames(mc));
-    expect_true("volume_fwhm10" %in% colnames(mc));
+    expect_equal(ncol(mc), 2); # 2 measures
     expect_true("thickness_fwhm10" %in% colnames(mc));
 })
 
 test_that("Concatination of standard space measures works for both hemispheres", {
-    subjects_dir = path.expand("~/data/tim_only")
+    nitools::download_optional_data();
+    subjects_dir = nitools::get_optional_data_filepath("subjects_dir");
     skip_if_not(dir.exists(subjects_dir), message="Test data missing.") # skip on travis
 
     num_verts_fsaverage_per_hemi = 163842;
 
-    subjects_list = c("tim", "timcopy");
-    measures = c("area", "thickness", "volume");
+    subjects_list = c("subject1", "subject2");
+    measures = c("area", "thickness");
     mc = group.concat.measures.standard(subjects_dir, subjects_list, measures, "both", fwhm="10");
     expect_equal(nrow(mc), num_verts_fsaverage_per_hemi * 2 * 2);  # 2 subjects with 2 hemis each
-    expect_equal(ncol(mc), 3); # 3 measures
+    expect_equal(ncol(mc), 2); # 2 measures
     expect_true("area_fwhm10" %in% colnames(mc));
-    expect_true("volume_fwhm10" %in% colnames(mc));
     expect_true("thickness_fwhm10" %in% colnames(mc));
 })
