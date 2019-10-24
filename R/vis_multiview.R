d078acdd18342b054c750a546da0e1b16c232a72
Tim Schäfer <ts+code@rcmd.org>
2019-10-24 10:21:47 +0200

    implement applying styles to visualizations

--- R/vis_multiview.R
+++ R/vis_multiview.R
@@ -46,8 +46,8 @@ vis.mult.coloredmeshes <- function(coloredmeshes, background="white", skip_all_n
 #' @param skip_all_na logical, whether to skip (i.e., not render) meshes in the list that have the property 'morph_data_was_all_na' set to TRUE. Defaults to TRUE. Practically, this means that a hemisphere for which the data was not given is not rendered, instead of being rendered in a single color.
 #' @export
 # @keywords internal
-#' @importFrom rgl open3d bg3d wire3d
-vis.mult.coloredmeshes.stdview4 <- function(coloredmeshes, background="white", skip_all_na=TRUE) {
+#' @importFrom rgl open3d bg3d wire3d shade3d mfrow3d next3d text3d
+vis.mult.coloredmeshes.stdview4 <- function(coloredmeshes, background="white", skip_all_na=TRUE, style="default") {
 
     draw_labels = TRUE;
     label_shift_y = -20;
@@ -96,7 +96,7 @@ vis.mult.coloredmeshes.stdview4 <- function(coloredmeshes, background="white", s
     }
     rgl.viewpoint(-90, 0, fov=0, interactive=FALSE);
     if(draw_labels) {
-        text3d(0,label_shift_y,0,"lateral lh");
+        rgl::text3d(0,label_shift_y,0,"lateral lh");
     }
 
     # Create the upper right view
@@ -107,23 +107,24 @@ vis.mult.coloredmeshes.stdview4 <- function(coloredmeshes, background="white", s
         rotated_mesh = rgl::rotate3d(orig_mesh, rotation_angle, 1, 0, 0);
         rgl::shade3d(rotated_mesh, col = rh_meshes[[mesh_idx]]$col);
     }
-    rgl.viewpoint(90, 0, fov=0, interactive=FALSE);
+    rgl::rgl.viewpoint(90, 0, fov=0, interactive=FALSE);
     if(draw_labels) {
-        text3d(0,label_shift_y,0,"lateral rh");
+        rgl::text3d(0,label_shift_y,0,"lateral rh");
     }
 
 
     # Create the lower left view
     rgl::next3d();
-    for (mesh_idx in seq_len(length(lh_meshes))) {     # usually this will only run once for the single lh mesh.
-        orig_mesh = lh_meshes[[mesh_idx]]$mesh;
-        rotation_angle = pi/2;
-        rotated_mesh = rgl::rotate3d(orig_mesh, rotation_angle, 1, 0, 0);
-        rgl::shade3d(rotated_mesh, col = lh_meshes[[mesh_idx]]$col);
-    }
-    rgl.viewpoint(90, 0, fov=0, interactive=FALSE);
+    #for (mesh_idx in seq_len(length(lh_meshes))) {     # usually this will only run once for the single lh mesh.
+    #    orig_mesh = lh_meshes[[mesh_idx]]$mesh;
+    #    rotation_angle = pi/2;
+    #    rotated_mesh = rgl::rotate3d(orig_mesh, rotation_angle, 1, 0, 0);
+    #    rgl::shade3d(rotated_mesh, col = lh_meshes[[mesh_idx]]$col);
+    #}
+    vis.rotated.coloredmeshes(lh_meshes, pi/2, 1, 0, 0, style=style);
+    rgl::rgl.viewpoint(90, 0, fov=0, interactive=FALSE);
     if(draw_labels) {
-        text3d(0,label_shift_y,0,"medial lh");
+        rgl::text3d(0,label_shift_y,0,"medial lh");
     }
 
 
@@ -133,14 +134,84 @@ vis.mult.coloredmeshes.stdview4 <- function(coloredmeshes, background="white", s
         orig_mesh = rh_meshes[[mesh_idx]]$mesh;
         rotation_angle = pi/2;
         rotated_mesh = rgl::rotate3d(orig_mesh, rotation_angle, 1, 0, 0);
-        rgl::shade3d(rotated_mesh, col = rh_meshes[[mesh_idx]]$col);
+        #rgl::shade3d(rotated_mesh, col = rh_meshes[[mesh_idx]]$col);
+        rotated_cmesh = rh_meshes[[mesh_idx]];
+        rotated_cmesh$mesh = rotated_mesh;
+        vis.coloredmesh(rotated_cmesh);
     }
-    rgl.viewpoint(-90, 0, fov=0, interactive=FALSE);
+    rgl::rgl.viewpoint(-90, 0, fov=0, interactive=FALSE);
     if(draw_labels) {
-        text3d(0,label_shift_y,0,"medial rh");
+        rgl::text3d(0,label_shift_y,0,"medial rh");
     }
 
 
 
     rgl::highlevel(integer()); # To trigger display as rglwidget
+}
+
+
+#' @keywords internal
+vis.rotated.coloredmeshes <- function(coloredmeshes, rotation_angle, ax, ay, az, style="default") {
+    for (mesh_idx in seq_len(length(coloredmeshes))) {     # usually this will only run once for the single mesh of a hemisphere.
+        orig_cmesh = coloredmeshes[[mesh_idx]];
+        orig_mesh = orig_cmesh$mesh;
+        rotated_mesh = rgl::rotate3d(orig_mesh, rotation_angle, ax, ay, az);
+        rotated_cmesh = orig_cmesh;         # copy coloredmesh
+        rotated_cmesh$mesh = rotated_mesh;  # replace inner mesh with rotated version
+        vis.coloredmesh(rotated_cmesh, style=style);
+    }
+}
+
+#' @title Draw a coloredmesh using a style.
+#'
+#' @param cmesh, a coloredmesh. A coloredmesh is a named list as returned by the coloredmesh.from.* functions. It has the entries 'mesh' of type tmesh3d, a 'col', which is a color specification for such a mesh.
+#'
+#' @param style, a named list of style parameters or a string specifying an available style by name (e.g., 'shiny'). Defaults to 'default', the default style.
+#'
+#' @keywords internal
+vis.coloredmesh <- function(cmesh, style="default") {
+    if(is.list(style)) {
+        style_params = style;
+    } else if (is.character(style)) {
+        style_params = get.rglstyle(style);
+    } else {
+        stop("Parameter 'style' must be a named list of style parameters or a string specifying an available style by name (e.g., 'default' or 'shiny').");
+    }
+    do.call(rgl::shade3d, c(list(cmesh$mesh, col=cmesh$col), style_params));
+}
+
+
+#' @title Get the default visualization style parameters as a named list.
+#'
+#' @param style, string. A style name. Available styles are one of: "default", "shiny".
+#'
+#' @keywords internal
+get.rglstyle <- function(style) {
+    if(style == "default") {
+        return(get.rglstyle.default());
+    } else if (style == "shiny") {
+        return(get.rglstyle.shiny());
+    } else {
+        stop(sprintf("No such style: '%s'.\n", style));
+    }
+}
+
+
+#' @title Get the default visualization style parameters as a named list.
+#'
+#' @return named list, style parameters that can be passed to [rgl::shade3d()] via [base::do.call()].
+#'
+#' @keywords internal
+get.rglstyle.default <- function() {
+    return(list("shininess"=50, specular="black"));
+}
+
+
+#' @title Get a shiny visualization style.
+#'
+#' @return named list, style parameters that can be passed to [rgl::shade3d()] via [base::do.call()].
+#'
+#' @keywords internal
+get.rglstyle.shiny <- function() {
+    return(list("shininess"=50, specular="white"));
 }
