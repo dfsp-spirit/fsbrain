e87a6896f0608921e5997b741f555c5ebb54cc2a
Tim Schaefer <ts+code@rcmd.org>
2020-01-14 18:04:03 +0100

    minor: remove print

--- R/vis_volume_3d.R
+++ R/vis_volume_3d.R
@@ -18,18 +18,17 @@ volvis.voxels <- function(volume, max_render=25000, render_every=8) {
     if(length(rendered_voxels) > max_render) {
         warning(sprintf("About to render %d voxels, but max_render is set to %d. Will stop early, the rest will not appear in the output. Try increasing one of the parameters 'max_render' or 'render_every'.\n", length(rendered_voxels), max_render));
         rendered_voxels = rendered_voxels[1:max_render];
-    } else {
-        cat(sprintf("Will render %d voxels.\n", length(rendered_voxels)));
     }
 
     if(nrow(voxel_crs) > 0) {
         voxel_crs = cbind(voxel_crs, 1);
         surface_ras = matrix(rep(0, length(rendered_voxels)*3), ncol=3);
+        vox2surface_ras_matrix = vox2ras_tkr();
         for(idx in seq(length(rendered_voxels))) {
             row_idx = rendered_voxels[idx];
-            surface_ras[idx,] = (vox2ras_tkr() %*% voxel_crs[row_idx,])[1:3];
+            surface_ras[idx,] = (vox2surface_ras_matrix %*% voxel_crs[row_idx,])[1:3];
         }
-        rgl::rgl.spheres(surface_ras, r = 0.5, color = "#0000ff", lit=FALSE);
+        rgl::rgl.spheres(surface_ras, r = 0.5, color = "#0000ff", lit=FALSE);   # boxes would be nice, but there seems to be no rgl function for them.
     } else {
         warning("No voxels to be visualized.");
     }
