542eaca19b2c57e55b18a3dff0789fbcb3fc1fbe
Tim Schaefer <ts+code@rcmd.org>
2020-01-14 18:22:15 +0100

    ad cube examples

--- tests/testthat/test-vis-volume-3d.R
+++ tests/testthat/test-vis-volume-3d.R
@@ -2,7 +2,7 @@
 
 test_that("A brain volume and an overlay can be merged", {
 
-    skip("This test has to be run manually and interactively.");
+    skip("This test has to be run manually and interactively. It requires an aseg.mgz file for the demo subject.");
 
     fsbrain::download_optional_data();
     subjects_dir = fsbrain::get_optional_data_filepath("subjects_dir");
@@ -14,4 +14,7 @@ test_that("A brain volume and an overlay can be merged", {
     ventricle_mask = vol.mask.from.segmentation(aseg, ventricle_aseg_codes);
 
     volvis.voxels(ventricle_mask);
+
+    # cmm = matrix(c(0, 0, 0, 0, 0, 255, 0, 255, 255, 0, 255, 0, 255, 255, 255, 255, 0, 0, 255, 255, 0, 255, 0, 255), ncol=3)
+    # rgl::shade3d(rgl::cube3d(vertices=cmm))
 })
