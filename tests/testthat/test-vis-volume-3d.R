cba8a6b7d0bcb72b8a42bbaea6d3ee4b3bf1f4ab
Tim Schaefer <ts+code@rcmd.org>
2020-01-15 12:54:14 +0100

    construct triangles of a 3D cube

--- tests/testthat/test-vis-volume-3d.R
+++ tests/testthat/test-vis-volume-3d.R
@@ -15,6 +15,61 @@ test_that("A brain volume and an overlay can be merged", {
 
     volvis.voxels(ventricle_mask);
 
-    # cmm = matrix(c(0, 0, 0, 0, 0, 255, 0, 255, 255, 0, 255, 0, 255, 255, 255, 255, 0, 0, 255, 255, 0, 255, 0, 255), ncol=3)
-    # rgl::shade3d(rgl::cube3d(vertices=cmm))
+    # ---Draw a cube---
+    #cube_vertices = matrix(c(0, 0, 0, 0, 0, 1, 0, 1, 1, 0, 1, 0, 1, 1, 1, 1, 0, 0, 1, 1, 0, 1, 0, 1), ncol=3);
+    #cube = rgl::cube3d(vertices=cube_vertices);
+    #rgl::shade3d(rgl::translate3d(cube, 5, 5, 5));
+
+    # ---Use triangles to quickly draw many cubes---
+    tris_cube = matrix(c(-0.5, -0.5, 0, # the 2 front tris start
+                         -0.5, -0.5, 1,
+                         0.5, -0.5, 0,
+                         # tris 2
+                         0.5,-0.5,0,
+                         -0.5, -0.5,1,
+                         0.5,-0.5,1,
+                         # tris 3: the back starts (shifted by +1.0 on y axis compared to front)
+                         -0.5, 0.5, 0,
+                         -0.5, 0.5, 1,
+                         0.5, 0.5, 0,
+                         # tris 4
+                         0.5,0.5,0,
+                         -0.5, 0.5,1,
+                         0.5,0.5,1,
+                         # tris 5: the left side starts
+                         -0.5,0.5,0,
+                         -0.5,0.5,1,
+                         -0.5,-0.5,0,
+                         # tris 6
+                         -0.5,-0.5,0,
+                         -0.5,0.5,1,
+                         -0.5,-0.5,1,
+                         # tris 7: the right side starts (shifted by +1.0 on x axis compared to left side)
+                         0.5,0.5,0,
+                         0.5,0.5,1,
+                         0.5,-0.5,0,
+                         # tris 8
+                         0.5,-0.5,0,
+                         0.5,0.5,1,
+                         0.5,-0.5,1,
+                         # tris 9: the bottom starts
+                         -0.5,-0.5,0,
+                         -0.5,0.5,0,
+                         0.5,-0.5,0,
+                         # tris 10
+                         0.5,-0.5,0,
+                         -0.5,0.5,0,
+                         0.5,0.5,0,
+                         # tris 11: the top starts (shifted by +1.0 on z axis compared to the bottom)
+                         -0.5,-0.5,1,
+                         -0.5,0.5,1,
+                         0.5,-0.5,1,
+                         # tris 10
+                         0.5,-0.5,1,
+                         -0.5,0.5,1,
+                         0.5,0.5,1
+                         ), ncol=3, byrow = TRUE);          # create a cube with coords in range (0,0,0) to (1,1,1).
+
+
+    #rgl::triangles3d(tris_coords);
 })
