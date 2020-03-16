b866c821bd90a9fb8908ff9beae640ed5e441953
Tim Schäfer <ts+code@rcmd.org>
2020-03-16 08:56:51 +0100

    implement simple gray mean curvature rendering, first part of work towards #3

--- R/vis_surface_background.R
+++ R/vis_surface_background.R
@@ -3,7 +3,24 @@
 # The background can be a single color or a FreeSurfer-style gray/dark-gray binarized mean curvature pattern that
 # gives the viewer a rough orientation with respect to gyri and sulci.
 
+
 #' @title Compute mean curv surface color layer.
+#'
+#' @param subjects_dir character string, the FreeSurfer SUBJECTS_DIR.
+#'
+#' @param subject_id character string, the subject identifier.
+#'
+#' @param hemi character string, one of 'lh', 'rh', or 'both'. The latter will merge the data for both hemis into a single vector.
+#'
+#' @param surface string. The display surface. E.g., "white", "pial", or "inflated". Defaults to "white".
+#'
+#' @param bin_colors vector of two character strings, the two colors to use.
+#'
+#' @param bin_thresholds vector of 2 double values, the curvature threshold values used to separate gyri from sulci.
+#'
+#' @return vector of color strings, one color per surface vertex. The coloring separates gyri from sulci.
+#'
+#' @seealso You can plot the return value using \code{\link[fsbrain]{vis.color.on.subject}}.
 #'
 #' @export
 background.mean.curvature <- function(subjects_dir, subject_id, hemi="both", cortex_only=FALSE, bin_colors=c('#898989', '#5e5e5e'), bin_thresholds=c(-0.1, 0.1)) {
@@ -13,3 +30,4 @@ background.mean.curvature <- function(subjects_dir, subject_id, hemi="both", cor
     color_layer[gyri_vertices] = bin_colors[2];
     return(color_layer);
 }
+
--- tests/testthat/test-vis-surface-background.R
+++ tests/testthat/test-vis-surface-background.R
@@ -5,10 +5,14 @@ test_that("A mean curvature color layer can be loaded", {
     subjects_dir = fsbrain::get_optional_data_filepath("subjects_dir");
     skip_if_not(dir.exists(subjects_dir), message="Test data missing.");
 
-    bgcol = background.mean.curvature(subjects_dir, "subject1", "both");
+    bgcol_lh = background.mean.curvature(subjects_dir, "subject1", "lh");
+    bgcol_rh = background.mean.curvature(subjects_dir, "subject1", "rh");
 
     num_verts_subject1_lh = 149244;
     num_verts_subject1_rh = 153333;
-    expect_equal(length(data), num_verts_subject1_lh + num_verts_subject1_rh);
+    expect_equal(length(bgcol_lh), num_verts_subject1_lh);
+    expect_equal(length(bgcol_rh), num_verts_subject1_rh);
+
+    vis.color.on.subject(subjects_dir, 'subject1', bgcol_lh, bgcol_rh);
 })
 
