d550d70b7cc909708f90c9633a168fa2d4206787
Tim Schaefer <ts+code@rcmd.org>
2020-07-01 20:44:36 +0200

    prepare generalp[lot funct

--- R/vis_group.R
+++ R/vis_group.R
@@ -24,7 +24,7 @@ recycle <- function(x, times) {
 }
 
 
-#' @title Plot annotation for a group of subjects.
+#' @title Plot atlas annotation for a group of subjects.
 #'
 #' @inheritParams vis.subject.annot
 #'
@@ -32,14 +32,46 @@ recycle <- function(x, times) {
 #'
 #' @param view_angles see \code{\link{get.view.angle.names}}.
 #'
-#' @param output_img character string, the file path for the output image
+#' @param output_img character string, the file path for the output image. Should end with '.png'.
 #'
 #' @param num_per_row positive integer, the number of tiles per row.
 #'
-#' @note The images are plotted row-wise, in the order in which they appear in the 'subject_id' parameter.
+#' @note The subjects are plotted row-wise, in the order in which they appear in the 'subject_id' parameter.
 #'
 #' @export
-vis.group.annot <- function(subjects_dir, subject_id, atlas, view_angles = 'sd_dorsal', output_img='fsbrain_group_annot.png', num_per_row = 10L) {
+vis.group.annot <- function(subjects_dir, subject_id, atlas, view_angles = 'sd_dorsal', output_img='fsbrain_group_annot.png', num_per_row = 5L) {
+    num_subjects = length(subject_id);
+    subjects_dir = recycle(subjects_dir, num_subjects);
+    atlas = recycle(atlas, num_subjects);
+    rglactions = list('no_vis'=TRUE);
+    tdir = tempdir();
+    all_output_images = rep(NA, num_subjects);
+    for(subject_idx in seq.int(num_subjects)) {
+        cm = vis.subject.annot(subjects_dir[subject_idx], subject_id[subject_idx], atlas[subject_idx], rglactions = rglactions);
+        subject_output_img = file.path(tdir, sprintf("%s_part.png", subject_id[subject_idx]));
+        vislayout.from.coloredmeshes(cm, view_angles=view_angles, output_img=subject_output_img, silent=TRUE, grid_like=TRUE);
+        all_output_images[subject_idx] = subject_output_img;
+    }
+    return(invisible(arrange.brainview.images.grid(all_output_images, output_img=output_img, num_per_row = num_per_row, annotations = subject_id)));
+}
+
+
+#' @title Plot coloredmeshes for a group of subjects.
+#'
+#' @inheritParams vis.subject.annot
+#'
+#' @param subject_id vector of subject identifiers
+#'
+#' @param view_angles see \code{\link{get.view.angle.names}}.
+#'
+#' @param output_img character string, the file path for the output image. Should end with '.png'.
+#'
+#' @param num_per_row positive integer, the number of tiles per row.
+#'
+#' @note The subjects are plotted row-wise, in the order in which they appear in the 'subject_id' parameter.
+#'
+#' @export
+vis.group.coloredmeshes <- function(coloredmeshes, view_angles = 'sd_dorsal', output_img='fsbrain_group_annot.png', num_per_row = 5L, annotations = NULL) {
     num_subjects = length(subject_id);
     subjects_dir = recycle(subjects_dir, num_subjects);
     atlas = recycle(atlas, num_subjects);
