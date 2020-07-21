3f535dc7d29731a5f74cf7a784bdc7b9629e962d
Tim Schaefer <ts+code@rcmd.org>
2020-07-21 15:34:23 +0200

    add more unit tests for group.vis and other functions

---
+++ tests/testthat/test-brainview_magic.R
@@ -0,0 +1,24 @@
+
+test_that("Creating pub-ready figure export from coloredmeshes works.", {
+    skip_if(tests_running_on_cran_under_macos(), message = "Skipping on CRAN under MacOS, required test data cannot be downloaded.");
+    fsbrain::download_optional_data();
+    subjects_dir = fsbrain::get_optional_data_filepath("subjects_dir");
+    skip_if_not(dir.exists(subjects_dir), message="Test data missing.");
+
+    rand_data = rnorm(327684, 5, 1.5);
+    cm = vis.data.on.fsaverage(morph_data_both=rand_data, rglactions=list('no_vis'=T));
+    output_img = tempfile(fileext = ".png");
+    vis.export.from.coloredmeshes(cm, colorbar_legend='Random data', output_img = output_img);
+    vis.export.from.coloredmeshes(cm, colorbar_legend='Random data', output_img = output_img, large_legend = TRUE);
+
+    vis.export.from.coloredmeshes(cm, colorbar_legend='Random data', output_img = output_img, quality = 2L);
+    vis.export.from.coloredmeshes(cm, colorbar_legend='Random data', output_img = output_img, quality = 2L, large_legend = TRUE);
+
+    expect_true(is.fs.coloredmesh(cm$lh));
+    expect_true(is.fs.coloredmesh(cm$rh));
+    expect_true(is.hemilist(cm));
+    expect_true(file.exists(output_img));
+    unlink(output_img); # cleanup
+
+    expect_error(vis.export.from.coloredmeshes(cm, colorbar_legend='Random data', output_img = output_img, quality = 3L, large_legend = TRUE)); # invalid quality
+})
---
+++ tests/testthat/test-vis_group.R
@@ -0,0 +1,57 @@
+
+test_that("Visualizing a single image for a group of subjects works for native space morph data.", {
+    skip_if(tests_running_on_cran_under_macos(), message = "Skipping on CRAN under MacOS, required test data cannot be downloaded.");
+    fsbrain::download_optional_data();
+    subjects_dir = fsbrain::get_optional_data_filepath("subjects_dir");
+    skip_if_not(dir.exists(subjects_dir), message="Test data missing.");
+
+    subjects_list = c('subject1', 'subject2');
+    output_img = tempfile(fileext = ".png");
+
+    vis.group.morph.native(subjects_dir, subjects_list, 'thickness', output_img = output_img, num_per_row = 5L);
+    vis.group.morph.native(subjects_dir, subjects_list, 'thickness', output_img = output_img, num_per_row = 1L);
+
+    gd = group.morph.native(subjects_dir, subjects_list, 'thickness', hemi='both');
+    vis.data.on.group.native(subjects_dir, subjects_list, gd, output_img = output_img);
+
+    expect_true(file.exists(output_img));
+    unlink(output_img); # cleanup
+})
+
+
+test_that("Visualizing a single image for a group of subjects works for standard space morph data.", {
+    skip_if(tests_running_on_cran_under_macos(), message = "Skipping on CRAN under MacOS, required test data cannot be downloaded.");
+    fsbrain::download_optional_data();
+    subjects_dir = fsbrain::get_optional_data_filepath("subjects_dir");
+    skip_if_not(dir.exists(subjects_dir), message="Test data missing.");
+
+    subjects_list = c('subject1', 'subject2');
+    output_img = tempfile(fileext = ".png");
+
+    vis.group.morph.standard(subjects_dir, subjects_list, 'thickness', output_img = output_img, num_per_row = 5L);
+    vis.group.morph.standard(subjects_dir, subjects_list, 'thickness', output_img = output_img, num_per_row = 1L);
+
+    gd = group.morph.standard(subjects_dir, subjects_list, 'thickness');
+    if(dir.exists(file.path(subjects_dir, 'fsaverage'))) {
+        vis.data.on.group.standard(subjects_dir, 'fsaverage', gd, output_img = output_img);
+    }
+
+    expect_true(file.exists(output_img));
+    unlink(output_img); # cleanup
+})
+
+
+test_that("Visualizing a single image for a group of subjects works for atlas data.", {
+    skip_if(tests_running_on_cran_under_macos(), message = "Skipping on CRAN under MacOS, required test data cannot be downloaded.");
+    fsbrain::download_optional_data();
+    subjects_dir = fsbrain::get_optional_data_filepath("subjects_dir");
+    skip_if_not(dir.exists(subjects_dir), message="Test data missing.");
+
+    subjects_list = c('subject1', 'subject2');
+    output_img = tempfile(fileext = ".png");
+
+    vis.group.annot(subjects_dir, subjects_list, 'aparc', output_img = output_img);
+
+    expect_true(file.exists(output_img));
+    unlink(output_img); # cleanup
+})
