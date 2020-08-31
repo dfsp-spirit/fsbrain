bf6151b13b383224a57e2ad33a585aaeaa42543d
Tim Schäfer <ts+code@rcmd.org>
2020-08-31 14:48:47 +0200

    Add functions to compute shape descriptors based on k1 and k2

---
+++ R/curvature.R
@@ -0,0 +1,119 @@
+# Functions to compute various curvature-based mesh geometry measures or shape descriptors.
+# These function assume you already have the 2 principal curvatures at each vertex. If not, run mris_curvature or use some mesh library to get them.
+# See Shimony et al. (2016). Comparison of cortical folding measures for evaluation of developing human brain. Neuroimage, 125, 780-790.
+
+
+#' @title Computes principal curvatures according to 2 definitions from raw k1 and k2 values.
+#'
+#' @param k1_raw numerical vector, one of the two principal curvatures, one value per vertex
+#'
+#' @param k2_raw numerical vector, the other one of the two principal curvatures, one value per vertex
+#'
+#' @return a named 'principal_curvatures' list, with entries 'principal_curvature_k1': larger value of k1_raw, k2_raw. 'principal_curvature_k2': smaller value of k1_raw, k2_raw. 'principal_curvature_k_major': larger value of abs(k1_raw), abs(k2_raw). 'principal_curvature_k_minor': smaller value of abs(k1_raw), abs(k2_raw).
+#' @export
+principal.curvatures <- function(k1_raw, k2_raw) {
+     nv = length(k1_raw);
+     if(nv != length(k2_raw)) {
+         stop(sprintf("Vectors k1_raw and k2_raw must have same lengths, but found %d and %d.\n", nv, length(k2)));
+     }
+     res_list = list();
+     res_list$principal_curvature_k1 = ifelse(k1_raw >= k2_raw, k1_raw, k2_raw);
+     res_list$principal_curvature_k2 = ifelse(k1_raw < k2_raw, k1_raw, k2_raw);
+     res_list$principal_curvature_k_major = ifelse(abs(k1_raw) >= abs(k2_raw), k1_raw, k2_raw);
+     res_list$principal_curvature_k_minor = ifelse(abs(k1_raw) < abs(k2_raw), k1_raw, k2_raw);
+     class(res_list) = c(class(res_list), 'principal_curvatures');
+     return(res_list);
+}
+
+
+#' @title Get all shape descriptor names.
+#'
+#' @return vector of character strings, the names
+#' @export
+shape.descriptor.names <- function() {
+    all_measures = c('principal_curvature_k1', 'principal_curvature_k2', 'principal_curvature_k_major', 'principal_curvature_k_minor', 'mean_curvature', 'gaussian_curvature', 'intrinsic_curvature_index', 'negative_intrinsic_curvature_index', 'gaussian_l2_norm', 'absolute_intrinsic_curvature_index', 'mean_curvature_index', 'negative_mean_curvature_index' ,'mean_l2_norm', 'absolute_mean_curvature_index', 'folding_index', 'curvedness_index', 'shape_index', 'shape_type', 'area_fraction_of_intrinsic_curvature_index', 'area_fraction_of_negative_intrinsic_curvature_index', 'area_fraction_of_mean_curvature_index', 'area_fraction_of_negative_mean_curvature_index', 'sh2sh', 'sk2sk');
+    return(all_measures);
+}
+
+
+#' @title Computes geometric curvature-based descriptors.
+#'
+#' @param pc a 'principal_curvatures' data list, see \code{\link{principal.curvatures}} for details.
+#'
+#' @param descriptors vector of character strings, the descriptors you want. See \code{\link{shape.descriptor.names}} for all available names.
+#'
+#' @return dataframe of descriptor values, each columns contains one descriptor.
+#' @export
+shape.descriptors <- function(pc, descriptors=shape.descriptor.names()) {
+    all_measures = shape.descriptor.names();
+    df = data.frame("principal_curvature_k1"=pc$principal_curvature_k1, "principal_curvature_k2"=pc$principal_curvature_k2, "principal_curvature_k_major"=pc$principal_curvature_k_major, "principal_curvature_k_minor"=pc$principal_curvature_k_minor);
+    for(measure in descriptors) {
+        if(! measure %in% all_measures) {
+            stop(sprintf("Invalid curvature measure: '%s'.\n", measure));
+        }
+        if(measure %in% c('principal_curvature_k1', 'principal_curvature_k2', 'principal_curvature_k_major', 'principal_curvature_k_minor')) {
+            next;
+        }
+        if(measure == 'gaussian_curvature') {
+            df[[measure]] = gm.gaussian_curvature(pc);
+        } else if(measure == 'mean_curvature') {
+            df[[measure]] = gm.mean_curvature(pc);
+        } else if(measure == 'intrinsic_curvature_index') {
+            df[[measure]] = gm.intrinsic_curvature_index(pc);
+        } else if(measure == 'negative_intrinsic_curvature_index') {
+            df[[measure]] = gm.negative_intrinsic_curvature_index(pc);
+        } else if(measure == 'absolute_intrinsic_curvature_index') {
+            df[[measure]] = gm.absolute_intrinsic_curvature_index(pc);
+        } else if(measure == 'mean_curvature_index') {
+            df[[measure]] = gm.mean_curvature_index(pc);
+        } else if(measure == 'negative_mean_curvature_index') {
+            df[[measure]] = gm.negative_mean_curvature_index(pc);
+        } else if(measure == 'absolute_mean_curvature_index') {
+            df[[measure]] = gm.absolute_mean_curvature_index(pc);
+        } else if(measure == 'mean_l2_norm') {
+            df[[measure]] = gm.mean_l2_norm(pc);
+        } else if(measure == 'gaussian_l2_norm') {
+            df[[measure]] = gm.gaussian_l2_norm(pc);
+        } else if(measure == 'folding_index') {
+            df[[measure]] = gm.folding_index(pc);
+        } else if(measure == 'curvedness_index') {
+            df[[measure]] = gm.curvedness_index(pc);
+        } else if(measure == 'area_fraction_of_mean_curvature_index') {
+            df[[measure]] = gm.area_fraction_of_mean_curvature_index(pc);
+        } else if(measure == 'area_fraction_of_negative_mean_curvature_index') {
+            df[[measure]] = gm.area_fraction_of_negative_mean_curvature_index(pc);
+        } else if(measure == 'area_fraction_of_intrinsic_curvature_index') {
+            df[[measure]] = gm.area_fraction_of_intrinsic_curvature_index(pc);
+        } else if(measure == 'area_fraction_of_negative_intrinsic_curvature_index') {
+            df[[measure]] = gm.area_fraction_of_negative_intrinsic_curvature_index(pc);
+        } else if(measure == 'sh2sh') {
+            df[[measure]] = gm.sh2sh(pc);
+        } else if(measure == 'sk2sk') {
+            df[[measure]] = gm.sk2sk(pc);
+        } else if(measure == 'shape_index') {
+            df[[measure]] = gm.shape_index(pc);
+        } else if(measure == 'shape_type') {
+            df[[measure]] = gm.shape_type(pc);
+        } else {
+            stop(sprintf("Unhandled descriptor: '%s'\n", measure));   # nocov
+        }
+    }
+    return(df);
+}
+
+
+
+#' @title Compute the respective curvature measure.
+#'
+#' @inheritParams geom.curvatures
+#'
+#' @keywords internal
+gm.gaussian_curvature <- function(pc) { return(pc$principal_curvature_k_major * pc$principal_curvature_k_minor) };
+
+
+#' @title Compute the respective curvature measure.
+#'
+#' @param pc a 'principal_curvatures' list, see \code{\link{principal.curvatures}}.
+#'
+#' @keywords internal
+gm.mean_curvature <- function(pc) { return((pc$principal_curvature_k_major + pc$principal_curvature_k_minor) / 2.0) };
---
+++ tests/testthat/test-curvature.R
@@ -0,0 +1,23 @@
+# tests for shape descriptor computation
+
+test_that("Shape descriptors can be computed", {
+    skip_if(tests_running_on_cran_under_macos(), message = "Skipping on CRAN under MacOS, required test data cannot be downloaded.");
+
+    fsbrain::download_optional_data();
+    subjects_dir = fsbrain::get_optional_data_filepath("subjects_dir");
+    subject_id = 'subject1';
+
+    # Load data
+    k1 = subject.morph.native(subjects_dir, subject_id, 'white.max', hemi='both');
+    k2 = subject.morph.native(subjects_dir, subject_id, 'white.min', hemi='both');
+
+    # Get principal curvature info
+    pc = principal.curvatures(k1, k2);
+
+    # Visualize k1
+    vis.data.on.subject(subjects_dir, subject_id, morph_data_both = pc$principal_curvature_k1, rglactions = list('trans_fun'=clip.data));
+
+    # Compute shape descriptors
+    shape = shape.descriptors(pc, descriptors = c('mean_curvature', 'gaussian_curvature'));
+    expect_true(is.data.frame(shape));
+})
