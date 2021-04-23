504f2f06304ae7bb176b6f2569f1ae9dfd948048
Tim Schäfer <ts+code@rcmd.org>
2021-04-23 21:51:20 +0200

    add FAQ entry on transparent background images

--- tests/testthat/test-vis_multiview.R
+++ tests/testthat/test-vis_multiview.R
@@ -403,7 +403,7 @@ test_that("We can export a PNG with background transparency from a tight layout 
 
     mkc = list('col.na'="#FEFEFE", 'colFn'=viridis::viridis, 'n'=200);
     coloredmeshes = vis.subject.morph.standard(subjects_dir, "subject1", "sulc", cortex_only=TRUE, views=NULL, makecmap_options = mkc);
-    vis.export.from.coloredmeshes(coloredmeshes, colorbar_legend = "sulcal depth [mm]", transparency_color = "white")
+    vis.export.from.coloredmeshes(coloredmeshes, colorbar_legend = "sulcal depth [mm]", transparency_color = "white");
 
     expect_equal(1L, 1L); # Empty tests will be skipped by testthat.
     close.all.rgl.windows();
