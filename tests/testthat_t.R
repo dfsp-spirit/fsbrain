7a8db8d8af82c7601d819695aa7d780d7dc8586b
Tim Schäfer <ts+code@rcmd.org>
2021-04-26 13:43:35 +0200

    delete unmatched testset file

--- tests/testthat_t.R
+++
@@ -1,6 +0,0 @@
-library(testthat)
-library(fsbrain)
-
-# Break up tests to allow tests to run longer than 10 min in travis,
-# see https://github.com/travis-ci/travis-ci/issues/3849#issuecomment-345686242.
-test_check("fsbrain", filter = "^[t]")
