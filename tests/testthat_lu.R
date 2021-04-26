61ef56c44840832e42095c6690e4752c8f74691a
Tim Schäfer <ts+code@rcmd.org>
2021-04-26 11:42:34 +0200

    split tests into even more chunks

--- tests/testthat_lu.R
+++ tests/testthat_lu.R
@@ -3,4 +3,4 @@ library(fsbrain)
 
 # Break up tests to allow tests to run longer than 10 min in travis,
 # see https://github.com/travis-ci/travis-ci/issues/3849#issuecomment-345686242.
-test_check("fsbrain", filter = "^[a-k]")
+test_check("fsbrain", filter = "^[l-u]")
