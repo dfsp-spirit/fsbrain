ac7436508961ed24748ac81927fb9de589292a54
Tim Schaefer <ts+code@rcmd.org>
2021-05-04 09:30:25 +0200

    finish geodesic test script

--- web/examples/brain_surface_geodesic.R
+++ web/examples/brain_surface_geodesic.R
@@ -1,4 +1,7 @@
 #!/usr/bin/env Rscript
+#
+# This demo script illustrates how to compute geodesic distances on
+# brain surface meshes using the Rvcg package (https://github.com/zarquon42b/Rvcg).
 
 devtools::install_github("dfsp-spirit/fsbrain"); # > 0.4.3
 devtools::install_github("dfsp-spirit/freesurferformats"); # > 0.1.14
@@ -19,32 +22,33 @@ brain_hemispheres = fsbrain::subject.surface(subjects_dir, subject_id, "white", 
 lh_tmesh3d = rgl::tmesh3d(c(t(brain_hemispheres$lh$vertices)), c(t(brain_hemispheres$lh$faces)), homogeneous=FALSE);
 #rh_tmesh3d = rgl::tmesh3d(c(t(brain_hemispheres$rh$vertices)), c(t(brain_hemispheres$rh$faces)), homogeneous=FALSE);
 
-lh_vertex_idx_precentral_gyrus = 32258;  # on precentral gyrus (red in the image)
-lh_vertex_idx_central_sulcus = 32220; # at bottom of central sulcus (green)
-lh_vertex_idx_postcentral_gyrus = 17792; # on postcentral gyrus (blue)
+lh_vertex_idx_precentral_gyrus = 32258;  # on precentral gyrus         (red in the image plotted later)
+lh_vertex_idx_central_sulcus = 32220;    # at bottom of central sulcus (green)
+lh_vertex_idx_postcentral_gyrus = 17792; # on postcentral gyrus        (blue)
 
 lh_source_point = brain_hemispheres$lh$vertices[lh_vertex_idx_precentral_gyrus, ];
-#freesurferformats::closest.vert.to.point(brain_hemispheres$lh, lh_source_point);
-lh_mid_point = brain_hemispheres$lh$vertices[lh_vertex_idx_postcentral_gyrus, ];
+lh_mid_point = brain_hemispheres$lh$vertices[lh_vertex_idx_central_sulcus, ];
 lh_destination_point = brain_hemispheres$lh$vertices[lh_vertex_idx_postcentral_gyrus, ];
+#freesurferformats::closest.vert.to.point(brain_hemispheres$lh, lh_source_point);
 
 # Optional: show source and dest points on brain surface.
-highlight.vertices.on.subject(subjects_dir, subject_id, verts_lh = c(lh_source_vertex_idx, lh_destination_vertex_sulc_idx, lh_destination_vertex_idx), verts_rh = NULL, views = "si", color_verts_lh = c("#FF0000", "#00FF00", "#0000FF"));
+highlight.vertices.on.subject(subjects_dir, subject_id, verts_lh = c(lh_vertex_idx_precentral_gyrus, lh_vertex_idx_central_sulcus, lh_vertex_idx_postcentral_gyrus), verts_rh = NULL, views = "si", color_verts_lh = c("#FF0000", "#00FF00", "#0000FF"));
 
-### Test one: from point red to blue.
+########## Test one: from point red to blue. ##########
 
 # Compute geodesic distance along mesh (through the central sulcus: from red down along the gyral wall to green, then back up to blue)
-Rvcg::vcgGeodist(lh_tmesh3d, lh_source_point, lh_destination_point);
+Rvcg::vcgGeodist(lh_tmesh3d, lh_source_point, lh_destination_point); # 36.40924
+# Compute Euclidian distance (air distance over the central sulcus), should be way shorter.
+euclidian.dist(lh_source_point, lh_destination_point); # 10.54589
+
+
+########## Test two: from point green to blue. The difference should be small.  ##########
+Rvcg::vcgGeodist(lh_tmesh3d, lh_mid_point, lh_destination_point); # 21.62851
 
 # Compute Euclidian distance (air distance over the central sulcus), should be way shorter.
-euclidian.dist(lh_source_point, lh_destination_point);
+euclidian.dist(lh_mid_point, lh_destination_point); # 18.59061
 
 
-### Test two: from point green to blue. The difference should be small.
-Rvcg::vcgGeodist(lh_tmesh3d, lh_source_point, lh_destination_point);
-
-# Compute Euclidian distance (air distance over the central sulcus), should be way shorter.
-euclidian.dist(lh_source_point, lh_destination_point);
 
 
 
