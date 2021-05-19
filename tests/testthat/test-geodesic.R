7f4a7a026d36c6b21eead3043083c7e4237efa38
Tim Schäfer <ts+code@rcmd.org>
2021-05-19 18:08:05 +0200

    fix too many cols passed in test

--- tests/testthat/test-geodesic.R
+++ tests/testthat/test-geodesic.R
@@ -30,12 +30,12 @@ test_that("The geodesic color overlay for several vertices over a full brain can
     rh_surf = subject.surface(subjects_dir, subject_id, surface = "white", hemi = "rh");
     surf_hemilist = list('lh'=lh_surf, 'rh'=rh_surf);
     source_verts = c(500, 32258, 150000, 250000, 320000);
-    patch_colors = c("#FF0000", "#00FF00", "#0000FF", "#FFFF00", "#FF00FF", "#000000");
+    patch_colors = c("#FF0000", "#00FF00", "#0000FF", "#FFFF00", "#FF00FF");
 
     overlay_hemilist = geod.patches.color.overlay(surf_hemilist, vertex = source_verts, color = patch_colors);
 
     # Visualize
-    vis.color.on.subject(subjects_dir, subject_id, color_both = overlay_hemilist, views = "si");
+    vis.color.on.subject(subjects_dir, subject_id, color_both = overlay_hemilist, views = "si", surface = "white");
 
     testthat::expect_equal(1L, 1L); # only prevent test skipping for now.
 })
