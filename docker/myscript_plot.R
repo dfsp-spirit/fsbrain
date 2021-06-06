6482b43e9998239241be0fcdaaba04c966a02938
Tim Schäfer <ts+code@rcmd.org>
2021-06-06 13:55:54 +0200

    finish dockerscript that outputs image file

--- docker/myscript_plot.R
+++ docker/myscript_plot.R
@@ -11,7 +11,7 @@ library("fsbrain");
 library("freesurferformats");
 library("Rvcg");
 
-do_vis = FALSE; # whether to try opening an OpengL window and plot. Turn off on headless machines without any X11.
+do_vis = TRUE; # whether to try opening an OpengL window and plot. Turn off on headless machines without any X11.
 
 euclidian.dist <- function(x1, x2) sqrt(sum((x1 - x2) ^ 2));
 vertex.euclid.dist <- function(surf, v1, v2) { euclidian.dist(surf$vertices[v1, ], surf$vertices[v2, ]) };
