879a95525b31aa33449927adcdb75dcdea6a7c80
Tim Schäfer <tim.schaefer@ae.mpg.de>
2026-08-31 16:57:32 +0200

    NEW: add more functions to download optional data. Make clear for ALL optional data the copyright holders and the attributions.

--- R/optdata.R
+++ R/optdata.R
@@ -570,7 +570,7 @@ download_fsaverage6 <- function(accept_freesurfer_license=FALSE,
 
 #' @title Download atlas files for the fsaverage template subject.
 #'
-#' @description Download a set of cortical atlas files (annotations) defined in the space of the fsaverage template subject, based on a declarative manifest file shipped with this package. The atlases (e.g., Schaefer 100-1000, Brainnetome, HCP-MMP1, AAL3, aparc_conventional) are defined in fsaverage space, but they are not part of FreeSurfer and are not subject to the FreeSurfer license. This data is not required for the package to work.
+#' @description Download a set of cortical atlas files (annotations) defined in the space of the fsaverage template subject, based on a declarative manifest file shipped with this package. The atlases (e.g., Schaefer 100-1000, Brainnetome, HCP-MMP1, AAL3) are defined in fsaverage space, but they are not part of FreeSurfer and are not subject to the FreeSurfer license. This data is not required for the package to work.
 #'
 #' @param scheme character string, the URL scheme to use. Either `"https"` (the default) or `"http"`. Switching to `"http"` can be useful as a fallback if the HTTPS server is unreachable.
 #'
@@ -590,7 +590,7 @@ download_fsaverage_atlases <- function(scheme="https") {
 
 #' @title Download atlas files for the fs_LR 32k template.
 #'
-#' @description Download a set of cortical atlas files (annotations) defined in the space of the fs_LR 32k template (the HCP-style surface space), based on a declarative manifest file shipped with this package. The atlases (e.g., Schaefer 100-1000, Brainnetome, AAL3, aparc_conventional) are not part of FreeSurfer and are not subject to the FreeSurfer license. This data is not required for the package to work.
+#' @description Download a set of cortical atlas files (annotations) defined in the space of the fs_LR 32k template (the HCP-style surface space), based on a declarative manifest file shipped with this package. The atlases (e.g., Schaefer 100-1000, Brainnetome, AAL3) are not part of FreeSurfer and are not subject to the FreeSurfer license. This data is not required for the package to work.
 #'
 #' @inheritParams download_fsaverage_atlases
 #'
